9ada53d36307da8e5e7bac397834d33b1b1e1690
Mauricio Belforte <belymau@hotmail.com>
2025-08-11 20:28:39 -0300

    Se testea el modelo 1.5 flash porque el 2.5 flash no funciono con el codigo que estamos implementando

--- api/lib/modelosPorProveedor.js
+++ api/lib/modelosPorProveedor.js
@@ -13,11 +13,11 @@ export const modelosPorProveedor = {
         "qwen-2.5-coder-32b"
     ],
     gemini: [
-        "gemini-2.5-flash-latest", // La mejor opción de la familia Gemini para uso gratuito masivo.
         "gemini-1.5-flash-latest", // Buena opción si buscas un modelo rápido con generosos límites.
+        "gemini-1.5-pro-latest", // Muy potente, pero con límites de uso gratuito más estrictos.
+        "gemini-2.5-flash-latest", // La mejor opción de la familia Gemini para uso gratuito masivo.
         "gemini-2.5-pro-latest", // El más potente, pero con los límites de uso gratuito más bajos.
-        "gemma-2-9b-it", // Más tokens gratis, sin tantas restricciones de frecuencia.
-        "gemini-1.5-pro-latest" // Muy potente, pero con límites de uso gratuito más estrictos.
+        "gemma-2-9b-it" // Más tokens gratis, sin tantas restricciones de frecuencia.
     ],
     together: [
         "Qwen/Qwen3-235B-A22B-Instruct-2507-tput",
